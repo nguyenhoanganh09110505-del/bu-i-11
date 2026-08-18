55831c311e00d718a9181b88301f36ebd0ab7b53
nguyenhoanganh09110505-del <nguyenhoanganh09110505@gmail.com>
2026-08-18 20:10:38 +0900

    Add files via upload

---
+++ 11-3.js
@@ -0,0 +1,37 @@
+let number = Number(prompt("Nhập một số từ 0 đến 9:"));
+
+switch (number) {
+    case 0:
+        alert("số không");
+        break;
+    case 1:
+        alert("sô một");
+        break;
+    case 2:
+        alert("số hai");
+        break;
+    case 3:
+        alert("số ba");
+        break;
+    case 4:
+        alert("số bốn");
+        break;
+    case 5:
+        alert("số năm");
+        break;
+    case 6:
+        alert("số sáu");
+        break;
+    case 7:
+        alert("số bảy");
+        break;
+    case 8:
+        alert("số tám");
+        break;
+    case 9:
+        alert("số chín");
+        break;
+    default:
+        alert("Vui lòng nhập số từ 0 đến 9");
+
+}
